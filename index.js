f11a702e9e36fecbcda4c3f42169cddfa461f398
MerchantKhalid88 <mohammadkhr88@gmail;com>
2022-11-28 03:33:40 +0100

    verify

--- index.js
+++ index.js
@@ -60,15 +60,9 @@ async function run(){
 
 
                 const id = req.params.id;
-                const query = {category_id:ObjectId(id)}
+                const query = {_id:ObjectId(id)}
                 const car = await carCollection.find(query).toArray()
-                if(query === '6382b63c3a4c3911b8b9c4e6'){
-                    res.send(car)
-                }
-                 else{
-                    const selectedCar= car.filter(p=>p.category_id===id);
-                    res.send(selectedCar)
-                 }
+                res.send(car)
                    
                  })
 
